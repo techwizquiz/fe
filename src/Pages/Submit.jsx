6be46039ee0e214bc4f4179363fb711e13f26473
taylor-tokareff <taylortokareff@gmail.com>
2021-08-25 15:04:29 -0700

    submit page added some of the form

--- src/Pages/Submit.jsx
+++ src/Pages/Submit.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import styles from './Submit.css';
 
@@ -6,6 +7,35 @@ export default class Submit extends Component {
     return (
       <div>
         <form>
+
+          <label>Question</label>
+          <input></input>
+
+          <label>Possible Answer</label>
+          <input></input>
+          <input type="radio" id="correct-A" name="correct-A" value="correct-A"></input>
+          <label>Correct Answer?</label>
+
+          <label>Possible Answer</label>
+          <input></input>
+          <input type="radio" id="correct-B" name="correct-B" value="correct-B"></input>
+          <label>Correct Answer?</label>
+
+
+          <label>Possible Answer</label>
+          <input></input>
+          <input type="radio" id="correct-C" name="correct-C" value="correct-C"></input>
+          <label>Correct Answer?</label>
+
+
+          <label>Possible Answer</label>
+          <input></input>
+          <input type="radio" id="correct-D" name="correct-D" value="correct-D"></input>
+          <label>Correct Answer?</label>
+
+
+
+
 
 
 
