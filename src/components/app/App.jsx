e3bbd00c8cb173f44ced7b51c439306e96f53fa8
AngelDOrtiz <aortizghs@gmail.com>
2021-08-25 15:08:19 -0700

    added and started arranging the quiz page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '../../Pages/Home';
 import Header from '../../Pages/Header';
 import AboutUs from '../../Pages/AboutUs';
+import QuizPage from '../../Pages/QuizPage';
 
 export default function App() {
   return ( 
