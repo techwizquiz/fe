6d0dbc4cf71d1337fd21447c851bc31be0b7b6d1
taylor-tokareff <taylortokareff@gmail.com>
2021-08-26 10:05:14 -0700

    fixed radio button bug on submit page

--- src/Pages/Submit.jsx
+++ src/Pages/Submit.jsx
@@ -13,24 +13,24 @@ export default class Submit extends Component {
 
           <label>Possible Answer</label>
           <input></input>
-          <input type="radio" id="correct-A" name="correct-A" value="correct-A"></input>
+          <input type="radio" id="correct-A" name="correct" value="correct-A"></input>
           <label>Correct Answer?</label>
 
           <label>Possible Answer</label>
           <input></input>
-          <input type="radio" id="correct-B" name="correct-B" value="correct-B"></input>
-          <label>Correct Answer?</label>
-
-
-          <label>Possible Answer</label>
-          <input></input>
-          <input type="radio" id="correct-C" name="correct-C" value="correct-C"></input>
+          <input type="radio" id="correct-B" name="correct" value="correct-B"></input>
           <label>Correct Answer?</label>
 
 
           <label>Possible Answer</label>
           <input></input>
-          <input type="radio" id="correct-D" name="correct-D" value="correct-D"></input>
+          <input type="radio" id="correct-C" name="correct" value="correct-C"></input>
+          <label>Correct Answer?</label>
+
+
+          <label>Possible Answer</label>
+          <input></input>
+          <input type="radio" id="correct-D" name="correct" value="correct-D"></input>
           <label>Correct Answer?</label>
 
 
