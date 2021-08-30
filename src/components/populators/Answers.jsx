4998542befb3a88291a58e34af149eedae97ae19
AngelDOrtiz <aortizghs@gmail.com>
2021-08-30 13:37:48 -0700

    styled choice buttons

--- src/components/populators/Answers.jsx
+++ src/components/populators/Answers.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { useQuestions } from './QuestionProvider';
+import styles from './Answers.css';
 
 const Answers = ({ a, b, c, d }) => {
   const answers = useQuestions();
   
   const answersElement = answers.map((answer) => (
     <>
-      <button key={answer.a}>{answer.a}</button>
-      <button key={answer.b}>{answer.b}</button>
-      <button key={answer.c}>{answer.c}</button>
-      <button key={answer.d}>{answer.d}</button>
+      <button type="radio" className={styles.button} key={answer.a}>{answer.a}</button>
+      <button type="radio" className={styles.button} key={answer.b}>{answer.b}</button>
+      <button type="radio" className={styles.button} key={answer.c}>{answer.c}</button>
+      <button type="radio" className={styles.button} key={answer.d}>{answer.d}</button>
     </>
   ));
 
