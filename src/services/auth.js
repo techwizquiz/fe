bc07582169acef49d483b15512b21011751e1fd8
cmmerritt <christiane.merritt@gmail.com>
2021-08-31 16:04:20 -0700

    change import in auth from request to Request

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-import { get, post } from './request';
+import { get, post } from './Request';
 
 export const postSignup = (email, password) => {
   return post('/api/v1/auth/signup', { email, password });
