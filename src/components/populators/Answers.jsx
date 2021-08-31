b4217905bbcddd3a01731ea6071cee2d13243106
AngelDOrtiz <aortizghs@gmail.com>
2021-08-31 15:54:38 -0700

    styled up the answers choices

--- src/components/populators/Answers.jsx
+++ src/components/populators/Answers.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable max-len */
 import React, { useState } from 'react';
 import { useSetWinLose } from '../../services/SessionProvider';
+import styles from './Answers.css';
+
 
 
 const Answers = ({ a, b, c, d, answer }) => {
 
   const [selectedChoice, setSelectedChoice] = useState(null);
 
-  const { setWinLose, winLose } = useSetWinLose();
-
+  const { setWinLose, winLose } = useSetWinLose('neutral');
+ 
 
   const choice = (e) => {
     console.log('selectedCHOICE', selectedChoice, 'HAHAHAHA', answer);
     // setSeenQuestionIds: add new id to array of ids in state
-    if (selectedChoice === answer) {
+    if(selectedChoice === answer) {
       setWinLose('win');
     } else {
       setWinLose('lose');
@@ -24,17 +26,23 @@ const Answers = ({ a, b, c, d, answer }) => {
 
   return (
     <>
-      <div>
-        <label><input type="radio" name="answer" value="a" onClick={({ target }) => setSelectedChoice(target.value)} key={a} />A: <pre>{a}</pre></label>
+      <div className={styles.answersPage}>
+        <div className={styles.left}>
+          <label><button className={styles.button}  type="radio" name="answer" value="a" onClick={({ target }) => setSelectedChoice(target.value)} key={a}>A: <pre className={styles.neutral || winLose === 'win' ?
+            styles.win : styles.lose}>{a}</pre></button></label> 
 
-        <label><input type="radio" name="answer" value="b" onClick={({ target }) => setSelectedChoice(target.value)} key={b} />B: <pre>{b}</pre></label>
+          <label><button className={styles.button}  type="radio" name="answer" value="b" onClick={({ target }) => setSelectedChoice(target.value)} key={b}>B: <pre className={winLose === 'win' ?
+            styles.win : styles.lose}>{b}</pre></button></label> 
 
-        <label><input type="radio" name="answer" value="c" onClick={({ target }) => setSelectedChoice(target.value)} key={c} />C: <pre>{c}</pre></label>
+          <label><button className={styles.button}  type="radio" name="answer" value="c" onClick={({ target }) => setSelectedChoice(target.value)} key={c}>C: <pre className={winLose === 'win' ?
+            styles.win : styles.lose}>{c}</pre></button></label> 
 
-        <label><input type="radio" name="answer" value="d" onClick={({ target }) => setSelectedChoice(target.value)} key={d} />D: <pre>{d}</pre></label>
-
-        {/* <button onClick={choice}>Submit</button> */}
-        <button onClick={choice}>Submit</button>
+          <label><button className={styles.button}  type="radio" name="answer" value="d" onClick={({ target }) => setSelectedChoice(target.value)} key={d}>D: <pre className={winLose === 'win' ?
+            styles.win : styles.lose}>{d}</pre></button></label> 
+        </div>
+        <div className={styles.right}>
+          <button onClick={choice} className={styles.submitButton}>Submit</button>
+        </div>
 
       </div>
 
