5371d71b7fb901d1ca3d1dad5e53d24534ab5791
cmmerritt <christiane.merritt@gmail.com>
2021-09-01 11:31:59 -0700

    css for avatar selection on signup page

--- src/components/populators/Avatars.jsx
+++ src/components/populators/Avatars.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Avatars.css';
 import monster from '../../../assets/png/004-monster.png';
 import youngWizard from '../../../assets/png/054-wizard.png';
@@ -7,29 +7,30 @@ import goblin from '../../../assets/png/031-goblin.png';
 import skullWizard from '../../../assets/png/045-wizard.png';
 import octopus from '../../../assets/png/034-sea-monster.png';
 
-export const Avatars = ({ onChange }) => {
+export const Avatars = () => {
+
   return (
     <>
       <h4>Choose your avatar</h4>
       <div>
         <label className={styles.label}>
-          <input type="radio" name="avatar" value="monster" className={styles.radioButton} defaultChecked onChange={onChange} />
+          <input type="radio" name="avatar" value="monster" defaultChecked className={styles.radioButton} />
           <img className={styles.img} src={monster} />
         </label>
         <label className={styles.label}>
-          <input type="radio" name="avatar" value="youngWizard" className={styles.radioButton} onChange={onChange} /> 
+          <input type="radio" name="avatar" value="youngWizard" className={styles.radioButton} /> 
           <img className={styles.img} src={youngWizard} />
         </label>
         <label className={styles.label}>
-          <input type="radio" name="avatar" value="goblin" className={styles.radioButton} onChange={onChange} />
+          <input type="radio" name="avatar" value="goblin" className={styles.radioButton} />
           <img className={styles.img} src={goblin} />
         </label>
         <label className={styles.label}>
-          <input type="radio" name="avatar" value="skullWizard" className={styles.radioButton} onChange={onChange} />
+          <input type="radio" name="avatar" value="skullWizard" className={styles.radioButton} />
           <img className = {styles.img} src={skullWizard} />
         </label>
         <label className={styles.label}>
-          <input type="radio" name="avatar" value="octopus" className ={styles.radioButton} onChange={onChange} />
+          <input type="radio" name="avatar" value="octopus" className={styles.radioButton} />
           <img className={styles.img} src={octopus} />
         </label>
       </div>
